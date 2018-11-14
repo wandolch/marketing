947510838bd42f8208c62129b8ba7e0c022056b4
Ivan Nelyubov <nelyubovi@ae.com>
2018-11-14 17:43:23 +0300

    initital

---
+++ precache-manifest.1dc8b01f8501c8e88df219be60e1ddab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "94bd3dace7bb448c26e9",
+    "url": "/static/js/main.94bd3dac.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "94bd3dace7bb448c26e9",
+    "url": "/static/css/main.c91ab6ea.chunk.css"
+  },
+  {
+    "revision": "21879bb58e8c899508e84ae3938bb75c",
+    "url": "/index.html"
+  }
+];
